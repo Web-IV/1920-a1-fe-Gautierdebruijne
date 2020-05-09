7dbe00e014cef979b79c694f5d440e23c5bbb844
Gautier de Bruijne <gautier.debruijne.y7024@student.hogent.be>
2020-05-09 14:35:21 +0200

    Added routing to /meeting/list & /meeting/add

--- meetingapp/src/app/app.module.ts
+++ meetingapp/src/app/app.module.ts
@@ -4,6 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MeetingModule } from './meeting/meeting.module';
 import { MaterialModule } from './material/material.module';
+import { RouterModule, Routes } from '@angular/router';
+import { MeetingListComponent } from './meeting/meeting-list/meeting-list.component';
+import { AddMeetingComponent } from './meeting/add-meeting/add-meeting.component';
+
+const appRoutes: Routes = [
+  {path: 'meeting/list', component: MeetingListComponent},
+  {path: 'meeting/add', component: AddMeetingComponent},
+  {path: '', redirectTo: 'meeting/list', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { MaterialModule } from './material/material.module';
   imports: [
     BrowserModule,
     MeetingModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
