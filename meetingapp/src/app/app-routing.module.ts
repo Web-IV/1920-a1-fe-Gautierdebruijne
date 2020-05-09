6204f35dd229f90e1ae52d12a30781a03387c59a
Gautier de Bruijne <gautier.debruijne.y7024@student.hogent.be>
2020-05-09 19:13:10 +0200

    Added SelectivePreloadStrategy

--- meetingapp/src/app/app-routing.module.ts
+++ meetingapp/src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SelectivePreloadStrategy } from './SelectivePreloadStrategy';
 
 const appRoutes: Routes = [
   {
     path: 'meeting',
-    loadChildren: () => import('./meeting/meeting.module').then(mod => mod.MeetingModule)
+    loadChildren: () => import('./meeting/meeting.module').then(mod => mod.MeetingModule),
+    data: { preload: true }
   },
-  // {path: '', redirectTo: 'meeting/list', pathMatch: 'full'},
+  {path: '', redirectTo: 'meeting/list', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent}
 ];
 
@@ -16,7 +18,7 @@ const appRoutes: Routes = [
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})],
+    RouterModule.forRoot(appRoutes, {preloadingStrategy: SelectivePreloadStrategy})],
   exports: [RouterModule]
 })
 
